afdcbf4bff20d40d6788174a952e1652caa5c996
Madhurja Adak <madhurjaadak666@gmail.com>
2026-09-21 20:46:57 +0530

    Time: 11 ms (88.65%), Space: 54.5 MB (33.93%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+        int m = s1.length;
+        int n = t1.length;
+        int[][] dp = new int[m + 1][n + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1[i - 1] == t1[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
