7efcb9ba4462ab0099922a747615879b11ba0d35
Madhurja Adak <madhurjaadak666@gmail.com>
2026-09-23 08:30:31 +0530

    Time: 17 ms (88.5%), Space: 58.8 MB (99.95%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for (int i:nums){
+            set.add(i);
+        }
+       if (set.size()==nums.length){
+           return false;
+       }else return true;
+    }
+}
