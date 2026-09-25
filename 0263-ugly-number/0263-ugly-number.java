18e32b545b320e3cefba3b928e6caebcc5591abd
Madhurja Adak <madhurjaadak666@gmail.com>
2026-09-25 19:09:05 +0530

    Time: 1 ms (99.15%), Space: 41.2 MB (99.98%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if (n == 0) {
+            return false;
+        }
+        int[] primes = {2, 3, 5};
+        for (int i : primes) {
+            while (n % i == 0) {
+                n /= i;
+            }
+        }
+        if(n==1) return true;
+        else return false;
+    }
+}
